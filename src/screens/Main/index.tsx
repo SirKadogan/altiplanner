bec6481a2667df9655ca7081198f7aaa9d2be3d1
sirkadogan <thaleskenne@hotmail.com>
2021-03-13 15:33:04 -0300

    Adding styled components

--- src/screens/Main/index.tsx
+++ src/screens/Main/index.tsx
@@ -1,17 +1,24 @@
 import React, { useState } from "react";
 import "./Main.css";
 
+// Libraries
+
 // Components
 import Header from "../../components/Header";
 import Padding from "../../components/Padding";
+import Activity from "../../components/Activity";
 
 const Main = () => {
   return (
     <div className="App p-grid p-dir-col">
       <Header />
-      <div className="p-grid p-d-flex">
+      <div className="p-grid">
         <Padding />
-        <div className="App-body p-col-12 p-md-8"></div>
+        <div className="p-col-12 p-md-8 p-grid">
+          <Activity />
+          <Activity />
+          <Activity />
+        </div>
         <Padding />
       </div>
     </div>
