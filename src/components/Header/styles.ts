cf46807b50525c1e26a7373895d47a4e1fcbffee
sirkadogan <thaleskenne@hotmail.com>
2021-03-13 16:28:44 -0300

    Changed gradient

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -7,7 +7,7 @@ export const Header = styled.div`
   padding: 10px;
   background-color: ${COLORS.ACCENT};
   background: rgb(117,230,218);
-  background: linear-gradient(90deg, rgba(117,230,218,1) 0%, rgba(190,255,248,1) 100%);
+  background: linear-gradient(90deg, rgba(117,230,218,1) 0%, rgba(173,252,244,1) 100%);
   box-shadow: 0px 5px 10px 0px rgba(0,0,0,0.25);
   display: flex;
   align-items: center;
