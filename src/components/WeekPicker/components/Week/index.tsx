89a5731984d15aeb614aa61d667ce5bb1699d95e
sirkadogan <thaleskenne@hotmail.com>
2021-03-23 22:02:30 -0300

    Selecionando semana

--- src/components/WeekPicker/components/Week/index.tsx
+++ src/components/WeekPicker/components/Week/index.tsx
@@ -9,11 +9,12 @@ import { WeekProps } from '../../index';
 interface WeekCardProps {
   active?: boolean;
   week: WeekProps;
+  onClick: () => void;
 }
 
-const Week: React.FC<WeekCardProps> = ({ week }) => {
+const Week: React.FC<WeekCardProps> = ({ week, active, onClick }) => {
   return (
-    <Container active={true}>
+    <Container active={active} onClick={onClick}>
       <Span>Semana {week.weekNumber}</Span>
       <DateRange>{`${week.startOfWeek} - ${week.endOfWeek}`}</DateRange>
     </Container>
--- src/components/WeekPicker/index.tsx
+++ src/components/WeekPicker/index.tsx
@@ -21,7 +21,6 @@ const WeekPicker = () => {
     const startOfWeek = isoWeek.clone().weekday(0).format('DD/MM');
     const endOfWeek = isoWeek.clone().weekday(6).format('DD/MM');
 
-    console.log({ weekNumber, startOfWeek, endOfWeek });
     return {
       startOfWeek,
       endOfWeek,
@@ -29,28 +28,40 @@ const WeekPicker = () => {
     }
   }
 
-
   const currentWeek = moment().isoWeek();
   const [weeks, setWeeks] = useState<WeekProps[]>([]);
+  const [counter, setCounter] = useState(0);
+  const [selectedWeek, setSelectedWeek] = useState(currentWeek);
 
   useEffect(() => {
+    buildWeekArray(currentWeek + counter);
+  }, [counter]);
+
+
+  const buildWeekArray = (weekToBuild: number) => {
     const newWeeks = [
-      buildWeek(currentWeek - 1),
-      buildWeek(currentWeek),
-      buildWeek(currentWeek + 1)
+      buildWeek(weekToBuild - 1),
+      buildWeek(weekToBuild),
+      buildWeek(weekToBuild + 1)
     ];
     setWeeks(newWeeks);
-  }, []);
+  }
 
-
+  const handleArrowClick = (operation: string) => {
+    if (operation === '+') {
+      setCounter(counter + 1);
+    } else {
+      setCounter(counter - 1);
+    }
+  }
 
   return (
     <Container>
-      <FiChevronLeft />
+      <FiChevronLeft onClick={() => handleArrowClick('-')} />
       <WeekContainer>
-        {weeks.map(week => <Week week={week} />)}
+        {weeks.map(week => <Week week={week} active={week.weekNumber === selectedWeek} onClick={() => setSelectedWeek(week.weekNumber)} />)}
       </WeekContainer>
-      <FiChevronRight />
+      <FiChevronRight onClick={() => handleArrowClick('+')} />
     </Container>
   );
 }
